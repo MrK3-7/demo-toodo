c3ec690a3325dafe24c7d67225bf03acc52d2783
MrK3-7 <glavnyak9@gmail.com>
2024-09-19 19:18:58 +0500

    Installed json-server and made db.json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ToDo from './components/ToDo';
+
+const App = () => {
+
+  return (
+      <main className="h-screen grid place-items-center">
+        <ToDo/>
+      </main>
+  )
+}
+
+export default App;
